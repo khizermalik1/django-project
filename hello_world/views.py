a6e060a07bfddaa2a65e0b1671e65b699889aa5b
Khizer Khan <khizermalik@hotmail.co.uk>
2025-10-13 17:27:19 +0100

    adjust views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,8 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
  
 def index(request):
-
-    if request.method == "POST":
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse(request.method)
+    return HttpResponse("Hello, world!")
